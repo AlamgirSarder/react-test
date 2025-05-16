fc11706c462221ce6d6a5598ddca654fc1330cca
Alamgir Sarder <alamgirsarderinfo@gmail.com>
2025-05-17 01:32:01 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import Test from './assets/components/test/Test'
+
+ 
+
+function App() {
+ 
+
+  return (
+  <>
+  <Test />
+  </>
+  )
+}
+
+
+export default App
---
+++ src/assets/components/test/Test.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+
+function Test(){
+
+
+
+    return(
+        <>
+         <h1>Welcome to react project</h1>
+
+        
+        </>
+    )
+}
+
+
+export default Test;
